c53e1125f8be52712e6c0c11b1303cedbd5608a1
Preocts <preocts@preocts.com>
2024-06-21 21:53:56 -0400

    Break app down into responsibilities
    Start to build more structure to the project files

--- src/flask_playground/app.py
+++ src/flask_playground/app.py
@@ -15,24 +15,22 @@ from __future__ import annotations
 
 import csv
 import datetime
-import functools
 import os
 import pathlib
 import secrets
 import time
-from collections.abc import Callable
 from collections.abc import Generator
 from typing import Any
 
 import flask
 import svcs
 
+from ._constants import SECRET_KEY_ENV
+from ._constants import TEMP_FILE_DIRECTORY
+from ._decorators import check_expired
+from ._decorators import require_login
 from .pizzastore import Order
 from .pizzastore import PizzaStore
-
-TEMP_FILE_DIRECTORY = "temp_files"
-SECRET_KEY_ENV = "FLASK_APP_SECRET_KEY"
-SESSION_LENGTH_SECONDS = 300
 
 
 def _database_factory() -> Generator[PizzaStore, None, None]:
@@ -63,60 +61,6 @@ def construct_app() -> flask.Flask:
 
 
 app = construct_app()
-
-
-def require_login(func: Callable[..., flask.Response]) -> Callable[..., flask.Response]:
-    """Decorated functions will always require a valid session user."""
-
-    @functools.wraps(func)
-    def login_enforement(*args: Any, **kwargs: Any) -> flask.Response:
-        if not flask.session.get("usersession"):
-            # Do not reroute a user to a private path on return from login
-            if flask.request.path.startswith("/_"):
-                flask.session["return_url"] = flask.request.url_root
-            else:
-                flask.session["return_url"] = flask.request.url
-
-            # Return both a flask redirect and an htmx redirect header to cover both
-            # cases of js users and js avoiders.
-            resp = flask.make_response(flask.redirect(flask.url_for("login")))
-            resp.headers["HX-Redirect"] = flask.url_for("login")
-            return resp
-
-        return func(*args, **kwargs)
-
-    return login_enforement
-
-
-def check_expired(func: Callable[..., flask.Response]) -> Callable[..., flask.Response]:
-    """Decorated functions will always require an unexpired session."""
-
-    @functools.wraps(func)
-    def login_enforement(*args: Any, **kwargs: Any) -> flask.Response:
-        granted_at = flask.session["usersession"].get("granted_at", 0)
-        is_expired = int(time.time()) - granted_at > SESSION_LENGTH_SECONDS
-
-        if is_expired:
-            flask.session.pop("usersession", default=None)
-
-            # Do not reroute a user to a private path on return from login
-            if flask.request.path.startswith("/_"):
-                flask.session["return_url"] = flask.request.url_root
-                code = 403
-            else:
-                flask.session["return_url"] = flask.request.url
-                code = 302
-
-            # Return both a flask redirect and an htmx redirect header to cover both
-            # cases of js users and js avoiders.
-            resp = flask.make_response(flask.redirect(flask.url_for("logout"), code))
-            resp.headers["HX-Redirect"] = flask.url_for("logout")
-            resp.headers["HX-Refresh"] = "true"
-            return resp
-
-        return func(*args, **kwargs)
-
-    return login_enforement
 
 
 @app.errorhandler(404)
